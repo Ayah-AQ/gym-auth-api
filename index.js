e08ea28a95815b48155c57db203c9ede19ec4977
OmarHamada1996 <omer.jasser@gmail.com>
2023-07-05 17:53:53 +0000

    testing1

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 'use strict';
-require('dotenv').config();
-const port = process.env.PORT || 8000;
-const server = require('./src/server');
-const { db } = require('./src/models/index');
 
+require('dotenv').config(); // Import dotenv library to load environment variables
+const app = require('./src/server.js');
+const { db } = require('./src/models/index.js');
 
-db.sync()
-    .then(() => {
-        server.start(port, () => {
-            console.log(`server up on port ${port}`);
-        })
-    })
+const PORT = process.env.PORT || 3000;
+
+db.sync() // Sync the database models
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+  })
+  .catch((error) => {
+    console.error('Unable to connect to the database:', error);
+  });
